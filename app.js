94f65097941a4599ace72e55c29d561ec1e4ed54
EC2 Default User <ec2-user@ip-172-31-35-94.ap-northeast-1.compute.internal>
2022-07-05 07:44:05 +0000

    [Add]mouseove,mouseout機能を追加

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#FF0000'});
+  });
+});
+
+
+
+
+
