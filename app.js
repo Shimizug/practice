2cba632a0245e38d084dad415ebf17d9d188195a
EC2 Default User <ec2-user@ip-172-31-35-94.ap-northeast-1.compute.internal>
2022-07-05 08:10:08 +0000

    [Finish]jsプラグイン前までの学習終了

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 $(function(){
-  $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
 
 
-$(function(){
-  $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
-  });
-});
